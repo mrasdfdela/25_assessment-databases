7d7c8e4974f04ad6fc8612e61af191cfad257366
Jerry Hsu <Jerry>
2021-06-17 21:26:20 -0400

    Assessment: Databases, Part 3 - Step 1 & 2

--- playlist-app/models.py
+++ playlist-app/models.py
@@ -7,20 +7,29 @@ db = SQLAlchemy()
 
 class Playlist(db.Model):
     """Playlist."""
-
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = 'playlists'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text)
 
 
 class Song(db.Model):
     """Song."""
-
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = 'songs'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    artist = db.Column(db.Text, nullable=False)
 
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
+    __tablename__ = 'playlists_songs'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'))
+    username = db.Column(db.Integer, db.ForeignKey('songs.id'))
 
-    # ADD THE NECESSARY CODE HERE
+    playlist = db.relationship('Playlist', backref='playlist_song')
+    song = db.relationship('Song', backref='playlist_song')
 
 
 # DO NOT MODIFY THIS FUNCTION
